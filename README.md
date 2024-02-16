3c2c188932089304d95369eafc66c3857963fc4b
Ayush Prasad <aayush.prasad28@gmail.com>
2024-02-17 02:04:51 +0530

    2nd commit

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-#First react project in git😍
+# First react project in git😍
 
-Window + ; //for inserting emojis
+Window + ; for inserting emojis
+
+# parcel
+
+- Dev Build
+- Gives Local server
+- HMR (Hot module Replacement)
+- File watching algorithm - written in C++
+- Caching (loads faster by saving some files already)
+- Minification
+- Bundling
+- Image Optimization
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("h1", { id: "child" }, "H1 Tag"),
+  React.createElement("h1", { id: "child" }, "H1 Tag😍😍"),
   React.createElement("h2", { id: "child" }, "H2 Tag"),
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
-
-console.log(heading);
